9ed752f9c67f1a9981eeed788be662e67c9b932e
amna14 <amna@golf.id>
2023-07-05 19:22:52 +0700

    fixing

--- config/Database.js
+++ config/Database.js
@@ -2,7 +2,7 @@ import {Sequelize} from "sequelize";
 const db = new Sequelize('railway','root','1yq3YwrKykMBvAFhNGxR',{
     host: 'containers-us-west-164.railway.app',
     port: '6187',
-    dialect: "mysql"
+    dialect: require('mysql2')
 }) 
 
 export default db;
