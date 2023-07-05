dbc9b6050cc7ad669fa1d2739945de89c49d6116
amna14 <amna@golf.id>
2023-07-05 19:42:54 +0700

    fixing

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,10 @@
 import {Sequelize} from "sequelize";
+import mysql2 from "mysql2";
 const db = new Sequelize('railway','root','1yq3YwrKykMBvAFhNGxR',{
     host: 'containers-us-west-164.railway.app',
     port: '6187',
-    dialect: "mysql"
+    dialect: "mysql",
+    dialectModule: mysql2
 }) 
 
 export default db;
