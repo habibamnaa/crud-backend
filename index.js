02ccb36d6d26e3fdb76b2b934d540ef6e5f38b81
amna14 <amna@golf.id>
2023-07-06 22:42:44 +0700

    fixing code

--- index.js
+++ index.js
@@ -4,18 +4,11 @@ import cors from "cors";
 import ProductRoute from './routes/ProductRoute.js';
 
 const app = express();
-const cors = require('cors');
-const corsOptions ={
-    origin:'https://crud-backend-beta.vercel.app/', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+
+app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 app.use(FileUpload());
 app.use(ProductRoute);
-app.use(cors(corsOptions));
-
 
 app.listen(5000, ()=> console.log('Server Up and Running...'));
